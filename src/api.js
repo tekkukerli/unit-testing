bc11aca1a3d01ca57fca72135f3afbb3bc3f09c9
tekkukerli <kerlitekku@gmail.com>
2021-05-06 19:57:28 +0300

    creating the API client

---
+++ src/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://outside-in-dev-api.herokuapp.com/V06y0OScZBi3avMESUUEk9yzReSxFO1k',
+});
+
+const api = {
+    loadRestaurants() {
+        return client.get('/restaurants').then(response => response.data);
+    },
+  };
+  
+  export default api;
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import restaurants from './restaurants';
+import api from '@/api';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-  modules: {restaurants},
+  modules: {
+    restaurants: restaurants(api),
+  },
 });
