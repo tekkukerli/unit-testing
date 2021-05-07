7a23432cbbd4f058c43015e5b6b398634e3afc09
tekkukerli <kerlitekku@gmail.com>
2021-05-07 22:59:19 +0300

    unit testing the store

--- tests/unit/store/restaurants.spec.js
+++ tests/unit/store/restaurants.spec.js
@@ -6,7 +6,7 @@ describe('restaurants', () => {
   const localVue = createLocalVue();
   localVue.use(Vuex);
 
-  describe('load action', () => {
+  describe('load action', () => { 
     describe('initially', () => {
       let store;
 
@@ -107,4 +107,48 @@ describe('restaurants', () => {
     });
 
   });
+
+  describe('create action', () => {
+    const newRestaurantName = 'Sushi Place';
+    const existingRestaurant = {id: 1, name: 'Pizza Place'};
+    const responseRestaurant = {id: 2, name: newRestaurantName};
+  
+    let api;
+    let store;
+  
+    beforeEach(() => {
+      api = {
+        createRestaurant: jest.fn().mockName('createRestaurant'),
+      };
+      store = new Vuex.Store({
+        modules: {
+          restaurants: restaurants(api, {records: [existingRestaurant]}),
+        },
+      });
+    });
+  
+    it('saves the restaurant to the server', () => {
+      api.createRestaurant.mockResolvedValue(responseRestaurant);
+      store.dispatch('restaurants/create', newRestaurantName);
+      expect(api.createRestaurant).toHaveBeenCalledWith(newRestaurantName);
+    });
+
+    describe('when save succeeds', () => {
+      beforeEach(() => {
+        api.createRestaurant.mockResolvedValue(responseRestaurant);
+        store.dispatch('restaurants/create', newRestaurantName);
+      });
+    
+      it('stores the returned restaurant in the store', () => {
+        expect(store.state.restaurants.records).toEqual([
+          existingRestaurant,
+          responseRestaurant,
+        ]);
+      });
+    });
+
+  });
+
+  
+
 });
