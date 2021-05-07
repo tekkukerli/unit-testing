a1979ddcd7060261c2edc6d76448f9c610d7231d
tekkukerli <kerlitekku@gmail.com>
2021-05-07 22:20:02 +0300

    unit testing the component

---
+++ src/components/NewRestaurantForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <form @submit.prevent="handleSave">
+    <v-text-field
+        placeholder="Add Restaurant"
+        filled
+        type="text"
+        v-model="name"
+        data-testid="new-restaurant-name-field"
+    />
+    <v-btn
+        type="submit"
+        color="primary"
+        class="black--text"
+        data-testid="new-restaurant-submit-button"
+    >
+    Add
+  </v-btn>
+  </form>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+
+export default {
+  name: 'NewRestaurantForm',
+  data() {
+    return {
+        name: '',
+    };
+  },
+  methods: {
+    ...mapActions({
+      createRestaurant: 'restaurants/create',
+    }),
+    handleSave() {
+      this.createRestaurant(this.name);
+    },
+  },
+};
+</script>
