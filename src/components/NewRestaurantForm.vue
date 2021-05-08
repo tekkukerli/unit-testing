2b388bbd83825805f2a971809c64535f27354547
tekkukerli <kerlitekku@gmail.com>
2021-05-08 12:20:40 +0300

    fixing deploy error

--- src/components/NewRestaurantForm.vue
+++ src/components/NewRestaurantForm.vue
@@ -1,54 +1,53 @@
 <template>
-    <form @submit.prevent="handleSave">
+  <form @submit.prevent="handleSave">
     <v-alert
-        v-if="validationError"
-        type="error"
-        data-testid="new-restaurant-name-error"
+      v-if="validationError"
+      type="error"
+      data-testid="new-restaurant-name-error"
     >
-    Name is required.
+      Name is required.
     </v-alert>
     <v-alert
-        v-if="serverError"
-        type="error"
-        data-testid="new-restaurant-server-error"
+      v-if="serverError"
+      type="error"
+      data-testid="new-restaurant-server-error"
     >
-    The restaurant could not be saved. Please try again.
+      The restaurant could not be saved. Please try again.
     </v-alert>
     <v-row>
-        <v-col cols="9">
+      <v-col cols="9">
         <v-text-field
-            placeholder="Add Restaurant"
-            filled
-            type="text"
-            v-model="name"
-            data-testid="new-restaurant-name-field"
+          placeholder="Add Restaurant"
+          filled
+          type="text"
+          v-model="name"
+          data-testid="new-restaurant-name-field"
         />
-        </v-col>
-    <v-col cols="3">
+      </v-col>
+      <v-col cols="3">
         <v-btn
-            type="submit"
-            color="primary"
-            class="black--text"
-            block
-            data-testid="new-restaurant-submit-button"
+          type="submit"
+          color="primary"
+          class="black--text"
+          block
+          data-testid="new-restaurant-submit-button"
         >
-        Add
-    </v-btn>
-    </v-col>
-   </v-row>
+          Add
+        </v-btn>
+      </v-col>
+    </v-row>
   </form>
 </template>
 
 <script>
 import {mapActions} from 'vuex';
-
 export default {
   name: 'NewRestaurantForm',
   data() {
     return {
-        name: '',
-        validationError: false,
-        serverError: false,
+      name: '',
+      validationError: false,
+      serverError: false,
     };
   },
   methods: {
@@ -56,20 +55,19 @@ export default {
       createRestaurant: 'restaurants/create',
     }),
     handleSave() {
-        if (this.name) {
-            this.validationError = false;
-            this.serverError = false;
-            this.createRestaurant(this.name)
-                .then(() => {
-                this.name = '';
-                })
-                .catch(() => {
-                this.serverError = true;
-                });
-        } else {
-            this.validationError = true;
-        }
-       
+      if (this.name) {
+        this.validationError = false;
+        this.serverError = false;
+        this.createRestaurant(this.name)
+          .then(() => {
+            this.name = '';
+          })
+          .catch(() => {
+            this.serverError = true;
+          });
+      } else {
+        this.validationError = true;
+      }
     },
   },
 };
